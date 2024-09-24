7584384bfd5217cda8566633b957335e9c5bfb5f
Mohamed Shata <mohamadessam004@gmail.com>
2024-09-24 16:15:10 +0300

    user profile page initial setup

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUser } from "../../redux/reducers/usersSlice";
+import { useNavigate } from "react-router-dom";
 
 const UserProfile = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { currentUser } = useSelector((state) => state.users);
+
+  useEffect(() => {
+    const currentId = localStorage.id;
+    currentId == null && navigate("/");
+  }, []);
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="px-20">
+      <div>
+        <img />
+      </div>
+
+      <div className="p-6 text-left px-16">
+        <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-green-500 mb-2">
+          <span>{currentUser?.name}</span>
+        </h4>
+        <p className="block antialiased font-sans text-base leading-relaxed bg-clip-text undefined text-mainWhite dark:text-white font-medium">
+          Username : {currentUser?.name}
+        </p>
+        <p className="block antialiased font-sans text-base leading-relaxed bg-clip-text undefined text-mainWhite dark:text-white font-medium">
+          My name is {currentUser?.name}, I'm a male and I'm a member here.
+        </p>
+      </div>
+      <hr />
+      <div className="p-[4rem]"></div>
     </div>
   );
 };
